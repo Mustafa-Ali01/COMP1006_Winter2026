6a6f0c30b61ac0cf2749f02b359ee25f91531938
Ali <200643639@student.georgianc.on.ca>
2026-01-17 23:44:33 -0500

    INDEX.php

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,11 @@
-<?php 
-require "header.php"; 
-echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
-require "footer.php"; 
+<?php
+// including external files
+require_once 'car.php';
+require_once 'connect.php';
+
+
+$myCar = new Car("Toyota", "Camry", 2024);
+echo "<h1>Lab Results</h1>";
+echo "<p>" . $myCar->PullCarDetails() . "</p>";
+
+?>
