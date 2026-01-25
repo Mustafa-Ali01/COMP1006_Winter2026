9388a899f7ce0b3d8791fd62b200c19020dec8b5
Ali <200643639@student.georgianc.on.ca>
2026-01-24 20:56:49 -0500

    Update index.php

--- COMP1006_Winter2026-main/COMP1006_Winter2026-main/03/03_make_it_maintainable/maintainablecode/index.php
+++ COMP1006_Winter2026-main/COMP1006_Winter2026-main/03/03_make_it_maintainable/maintainablecode/index.php
@@ -5,4 +5,5 @@ $pageTitle = "My PHP Page";
 $year = 2026; //In most cases you could pull the year but i'm not entirely sure on how to do that
 
 // The one thing i learned from this lab was DRY (not making repeated code that can be done with includes as i've done in the lab)
+// I feel asthough that is the takeaway from this lab
 include 'includes/layout.php';
